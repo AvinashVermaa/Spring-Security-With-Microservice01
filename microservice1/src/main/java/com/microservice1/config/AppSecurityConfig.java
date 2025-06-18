2a25a6b3d4013775c29186c6c1c4224d7d4b3762
Avinash Verma <avinashverma657@gmail.com>
2025-06-18 19:40:50 +0530

    First Commit

---
+++ microservice1/src/main/java/com/microservice1/config/AppSecurityConfig.java
@@ -0,0 +1,50 @@
+package com.microservice1.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.microservice1.filter.JwtFilter;
+import com.microservice1.filter.JwtService;
+
+@Configuration
+public class AppSecurityConfig{
+	
+	@Autowired
+	private JwtFilter jwtFilter;
+	
+	private String[] publicEndPointUrl = {
+			"/v3/api-docs/**",
+	        "/swagger-ui/**",
+	        "/swagger-ui.html",
+	        "/swagger-resources/**",
+	        "/webjars/**",
+	        "/actuator/**", 
+	        "/eureka/**"	
+	};
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+		
+		http.authorizeHttpRequests(req->{
+			req.requestMatchers(publicEndPointUrl)
+			.permitAll()
+			.anyRequest()
+			.authenticated();
+		})
+		.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+		
+		return http.csrf().disable().build();
+	}
+	
+}
---
+++ microservice1/src/main/java/com/microservice1/filter/JwtService.java
@@ -0,0 +1,35 @@
+package com.microservice1.filter;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+@Service
+public class JwtService{
+	
+	private static final String SECRET_KEY = "secret@123(#*#)@_";
+	private static final long EXPIRATION_TIME = 86400000;
+	
+	
+	public String generateToken(String username,String role) {
+		return JWT.create()
+			   .withSubject(username)
+			   .withClaim("role", role)
+			   .withIssuer("AvinashDon")
+			   .withIssuedAt(new Date())
+			   .withExpiresAt(new Date(System.currentTimeMillis()+EXPIRATION_TIME))
+			   .sign(Algorithm.HMAC256(SECRET_KEY));
+	}
+	
+	public String validateTokenAndReturnUsername(String token) {
+		return JWT.require(Algorithm.HMAC256(SECRET_KEY))
+				.build()
+				.verify(token)
+				.getSubject();
+	}
+	
+	
+}
